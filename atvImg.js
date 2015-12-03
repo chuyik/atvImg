3ba90bb15e7dfa3eadce32fc31e1ac45016061ef
Edward Chu <crazyzyt@gmail.com>
2015-12-03 12:49:40 +0800

    support UMD

--- atvImg.js
+++ atvImg.js
@@ -173,6 +173,22 @@ function atvImg(options) {
   }
 }
 
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['$'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('$'));
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory(root.$);
+    }
+}(this, function ($) {
+    return atvImg;
+}));
 
 // work with jQuery/Zepto
 
